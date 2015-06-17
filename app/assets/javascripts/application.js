27aec4b8704ecaa062ea18b787782c056fd84b8e
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-17 03:25:48 +0000

    added image marker to location

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,15 +18,25 @@
 $(document).ready(function (){
 
     function initialize() {
+    var myLatlng = new google.maps.LatLng(32.782843, -96.7976736)
+     
       var mapOptions = {
-      center: { lat: -36.848738, lng: 174.752173},
+      center: { lat: 32.782843, lng: -96.7976736},
       zoom: 15,
       scrollwheel: false
       };
         
       var map = new google.maps.Map(document.getElementById('map-canvas'),
-                mapOptions);    
-    }
+                mapOptions);  
       
+      // var image = 'https://s-media-cache-ak0.pinimg.com/originals/d1/7c/6c/d17c6c87d77fa9e934922eb1d141ba35.jpg'
+      var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello'
+        });
+
+    }
+
     google.maps.event.addDomListener(window, 'load', initialize);
 });
